deb87eb6e4cd1fdbe7ac1395d956ee436cc4b259
C.N <cn.mbhalati@gmail.com>
2022-01-28 18:19:46 +0200

    Nearly finalised the game

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
-
-const pool = 4
-
 function computerPlay() {
-    randomValue = Math.floor(Math.random()*numbers)
-    if (randomValue === 1) {
-        alert('Rock!')
+    pool = 3
+    randomValue = Math.floor(Math.random()*pool)
+    if (randomValue === 0) {
+        move = 'rock'
+    } else if(randomValue === 1) {
+        move = 'paper'
     } else if(randomValue === 2) {
-        alert('Paper!')
-    } else if(randomValue === 3) {
-        alert('Scissors!')
+       move = 'scissors'
+    } else {
+        move = 'Invalid choice!'
     }
+    return move;
 }
+
+function playRound(playerChoice, computerSelection) {
+    playerSelection = playerChoice.toLowerCase()
+    if (playerSelection == computerSelection) {
+        result = `It's a draw! You both picked ${playerSelection}`
+    // If player wins
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        result = `You win! ${playerSelection} beats ${computerSelection}`
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        result = `You win! ${playerSelection} beats ${computerSelection}`
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        result = `You win! ${playerSelection} beats ${computerSelection}`
+
+    // If player loses
+    } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
+        result = `You lose! ${computerSelection} beats ${playerSelection}`
+    } else if (computerSelection == 'paper' && playerSelection == 'rock') {
+        result = `You lose! ${computerSelection} beats ${playerSelection}`
+    } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
+        result = `You lose! ${computerSelection} beats ${playerSelection}`
+    } else {
+        result = `Something went wrong. Yours:${playerSelection}, Computer:${computerSelection}.`
+    }
+
+    return result;
+}
+
+function game() {
+    playerScore = 0
+    computerScore = 0
+    for (let i = 0; i < 5; i++) {
+        playerMove = prompt("What's your move?")
+        computerMove = computerPlay()
+        thing = playRound(playerMove, computerMove)
+        if (thing.slice(0, 7) === 'You win') {
+            playerScore++
+            console.log(`You win! ${playerMove} beats ${computerMove}`)
+        } else if (thing.slice(0, 8) === 'You lose') {
+            computerScore++
+            console.log(`You lose! ${computerMove} beats ${playerMove}`)
+        } else {
+            console.log(`It's a draw! You both picked ${playerMove}`)
+            playerScore += 0.5
+            computerScore += 0.5
+        }
+    } 
+    
+    return `You: ${playerScore}, Computer: ${computerScore}`
+
+}
+
+// (thing.slice(0, 11) === "It's a draw")
